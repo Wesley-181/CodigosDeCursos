b26642bfc234ddff6a8c3fc1c3b574893cf54019
Wesley-181 <wesley.m.medeiros@hotmail.com>
2019-04-06 23:37:36 -0300

    Alteração logo.svg + Introdução JS

---
+++ PraticaJS/script.js
@@ -0,0 +1,99 @@
+//Comentário de uma linha em JS
+
+/* Comentário
+de várias 
+linhas em JS
+*/
+
+//alert("Ola mundo!"); //Exibe uma janela de "alerta" no navegador
+
+/* 
+var msg = "Hello World";
+alert(msg);		//Imprime o conteúdo da variável "msg"
+*/
+
+/* Atribuição de variáveis e operações aritméticas básicas
+var x = 5;
+var y = 3;
+
+var soma = x+y;
+var subtracao = x-y;
+var multiplicacao = x*y;
+var divisao = x/y;
+
+alert(soma);
+alert(subtraçao);
+alert(multiplicacao);
+alert(divisao);
+*/
+
+/* Estrutura Condicional em JS
+var a = 3;
+var b = 2;
+var c = "A";
+
+if (c%2 == 1) {
+	alert("Número Impar");
+}
+else if (c%2 == 0){
+	alert("Número Par");
+}
+else {
+	alert("Valor inválido");
+} */
+
+/* Estruturas de Repetição em JS 
+
+var n = 0;
+while (n < 5) {
+	alert(n);
+	n++;
+}
+
+for (var z = 0; z < 10; z++) {
+	alert(z);
+}
+*/
+
+/* Tipos de Dados + Array
+var ninteiro = 1;
+var nreal = 1.8;
+var cadeiaCaractere = "Tô chegando";
+var minhaLista = ["Melão", " Melancia", " Mel", 12345];
+
+alert(minhaLista[0]);
+alert(minhaLista[1]);
+alert(minhaLista[2]);
+alert(minhaLista);
+
+for (n in minhaLista) {
+	// Imprime as posições do Array alert(n);
+
+	// Imprime tudo que está no Array alert(minhaLista[n]);
+}
+*/
+
+/* Não exibe janela(para o usuário) apenas mostra a mensagem no "console" do navegador
+	console.log("Chamando..."); 
+*/
+
+
+/* Funções
+function soma(a, b){
+	console.log(a+b);
+}
+
+//soma(2, 2);
+
+function subtracao(a, b){
+	return a-b;
+}
+
+var s = subtracao(5, 3);
+//console.log(s);
+
+function multiplicacao(a, b){
+	return a*b;
+}
+console.log(multiplicacao(5,5))
+*/
