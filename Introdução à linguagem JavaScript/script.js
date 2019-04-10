28e7b6171e7ffbde56ce088a217847006ed4075a
Wesley-181 <wesley.m.medeiros@hotmail.com>
2019-04-10 00:11:20 -0300

    Intro JS

---
+++ Introdução à linguagem JavaScript/script.js
@@ -0,0 +1,67 @@
+//Comentário de uma linha
+
+/*
+Comentário 
+de 
+várias 
+linhas 
+*/
+
+/* alert("Ola Mundo!");
+
+var msg = 'Olá Mundo através da variável';
+alert(msg); */
+
+/* Operações matemáticas
+var a = 5;
+var b = 3;
+
+var soma = a+b;
+var subtracao = a-b;
+var multiplicacao = a*b;
+var divisao = a/b;
+
+alert(soma);
+alert(subtracao);
+alert(multiplicacao);
+alert(divisao);
+*/
+
+/* Estrutura condicional
+var a = 2
+var b = 3;
+
+if (b%2 == 1) {
+	alert("Número Impar");
+}else if (b%2 == 0) {
+	alert("Número Par");
+}else{
+	alert("Valor inválido!");
+}  */
+
+/*Estrutura de repetição
+
+//While
+
+var i = 0;
+
+while(i < 3){
+	alert(i);
+
+	i = i + 1;
+}
+
+// For
+
+for (var j = 10; j > 0; j--){
+	alert(j);
+} */
+
+//Tipos de Dados
+
+var numero = 6;
+var decimal = 4.5;
+var texto = "Olá Mundo!";
+var lista = ("laranja, maçã, banana");
+
+alert(lista[]);
