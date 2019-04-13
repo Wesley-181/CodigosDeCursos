8c4083d09bdda695a7f171e31574e2408b5de22b
Wesley-181 <wesley.m.medeiros@hotmail.com>
2019-04-12 22:04:37 -0300

    Função

---
+++ Apostila Linguagem C/3 - Função Simples.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int mensagem() {
+	printf("Hello World !");
+	return(0);
+}
+
+int main () {
+	
+	mensagem();
+	printf("\nCom Funcao() \n");
+	system("pause");
+	return(0);
+}
