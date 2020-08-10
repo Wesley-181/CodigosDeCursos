16b335df0fec854457848654ff80c753cbae49d8
Wesley <wesley.m.medeiros@hotmail.com>
2020-08-10 04:42:37 -0300

    TechCod Aula 4

---
+++ TechCod/projeto_comentarios/entrar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Sistema de Comentários</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/entrar.css">
+    </head>
+
+<body>
+    <span>Miriam<br>TECHCOD</span>
+    <form method="POST">
+        <h1>Acesse a sua conta</h1>
+        <img src="imagens/envelope.png">
+        <input type="email" name="email" autocomplete="off">
+        <img src="imagens/cadeado.png">
+        <input type="password" name="senha">
+        <input type="submit" value="ENTRAR">
+        <a href="cadastrar.php">Registre-se agora!</a>
+    </form>
+</body>
+</html>
